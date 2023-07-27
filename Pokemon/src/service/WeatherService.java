c1e8f731c6a60511d0a014230f755a16be26c3e3
orbaykahraman <orbaykahraman10@outlook.com>
2023-07-27 18:48:33 +0300

    Weather impact

---
+++ Pokemon/src/service/WeatherService.java
@@ -0,0 +1,13 @@
+package service;
+
+import model.Player;
+import model.WeatherForecast;
+
+public class WeatherService {
+
+    public void weatherImpact(WeatherForecast watherForecast, Player player){
+
+    }
+
+
+}
